5176c3511657da1a160dc17f18237f1e1d8004c7
Brendan Stanton <bmsenate@gmail.com>
2018-01-18 16:15:08 -0800

    added everything

---
+++ server.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let mongoose = require('mongoose');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/RaceR');
+
+
+let RaceSchema = new mongoose.Schema({
+	startLat: String,
+	startLong: String,
+	endLat: String,
+	endLong: String 
+})
+
+let Race = mongoose.model('Race', RaceSchema);
+
+
+app.get('/races', function(req, res){
+	console.log('YOU RACES!')
+	res.json({message: "All races!"})
+})
+
+app.post('/new/race', function(req, res){
+
+
+
+	console.log(req.bdy)
+
+	res.json({message: "You added a race!"})
+})
+
+app.listen(8003, function(){
+	console.log("App on 8003")
+})
+
+
