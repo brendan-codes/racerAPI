8df11458586570b25856847c7840cb2a81a385e2
Brendan Stanton <bmsenate@gmail.com>
2018-01-19 00:52:33 -0800

    updated models

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ let app = express();
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-mongoose.connect('mongodb://localhost/RaceR');
+mongoose.connect('mongodb://localhost/RaceR1');
 
 
 let RaceSchema = new mongoose.Schema({
+	raceName: String,
+	milesDistance: Number,
+	metersDistance: Number,
 	startLat: Number,
 	startLong: Number,
 	endLat: Number,
@@ -19,10 +22,13 @@ let RaceSchema = new mongoose.Schema({
 })
 
 let TimeSchema = new mongoose.Schema({
-	_raceid: String
+	_raceid: String,
 	startTime: String,
 	endTime: String
 })
+
+let names1 = ["Roquefort", "Havarti", "Provolone", "Gruyere", "Emmentaler", "Monterey Jack", "Brie", "Mozzarella", "Camembert", "Manchego", "Muenster", "Colby", "Asiago", "Cotija", "Ricotta"]
+let names2 = ["Run", "Gauntlet", "Dash", "Gambit", "Speedway", "Bend", "Corridor", "Derby", "Circuit", "Turnpike", "Route", "Way", "Ascent", "Exodus", "Trek", "Expedition"]
 
 let Race = mongoose.model('Race', RaceSchema);
 
@@ -35,6 +41,12 @@ app.get('/races', function(req, res){
 app.post('/new/race', function(req, res){
 	let newRace = new Race(req.body)
 	console.log(newRace);
+
+	// random name here
+	var randomCheese = names1[Math.floor(Math.random()*names1.length)];
+	var randomRace = names2[Math.floor(Math.random()*names2.length)];
+
+	let raceName = "The ${randomCheese} ${randomRace}"
 
 	newRace.save(function(err){
 		if(err){ res.json({error: "error"})}
